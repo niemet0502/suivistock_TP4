67191c1624ac3df8a4cfbc4cc76382b342fbea1e
niemet0502 <vincentmarius8@gmail.com>
2021-02-07 10:55:18 +0100

    changer le chemin et le type de SGBD dans le fichier Bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -10,14 +10,14 @@ $isDevMode = true;
 $proxyDir = null;
 $cache = null;
 $useSimpleAnnotationReader = false;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/entities/"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
 // or if you prefer yaml or XML
 //$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 
 // database configuration parameters
 $conn = array(
-    'driver' => 'pdo_sqlite',
+    'driver' => 'pdo_mysql',
     'path' => __DIR__ . '/db.sqlite',
 );
 
