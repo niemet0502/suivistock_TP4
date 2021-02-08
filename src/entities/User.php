7ccd486fca67baf59d413f06bcf2ae8e4b0fa5a8
niemet0502 <vincentmarius8@gmail.com>
2021-02-08 21:24:02 +0100

    ajout de la documentation dans l'entite User

--- src/entities/User.php
+++ src/entities/User.php
@@ -1,10 +1,33 @@
 <?php
+  use Doctrine\ORM\Mapping as ORM;
+
+  /**
+   * @ORM\Entity
+   * @ORM\Table(name="users")
+   */
   class User
   {
+    /**
+   * @ORM\Id
+   * @ORM\Column(type="integer")
+   * @ORM\GeneratedValue
+   */
     private $id;
+    /**
+   * @ORM\Column(type="string")
+   */
     private $nom;
+    /**
+   * @ORM\Column(type="string")
+   */
     private $prenom;
+    /**
+   * @ORM\Column(type="string")
+   */
     private $password;
+    /**
+   * @ORM\Column(type="id")
+   */
     private $etat;
 
     public function __construct(){
