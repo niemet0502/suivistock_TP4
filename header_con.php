980bc9bb9358497cd0cd072282de99d62406d0a4
niemet0502 <vincentmarius8@gmail.com>
2021-02-24 00:19:56 +0100

    main :: created file for the header of login page

---
+++ header_con.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Banque du Peuple</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <!-- Main Stylesheet File -->
+    <link href="http://127.128.1.1/BanqueDuPeupleOrm/public/css/styleCon.css" rel="stylesheet">
+
+</head>
+<body style="background: url(../img/img8.jpg) no-repeat center top;">
