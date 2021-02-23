ea2605dd50b13d54911332e6a86588d5a6046cf3
niemet0502 <vincentmarius8@gmail.com>
2021-02-24 00:08:01 +0100

    main :: added bootstrap config class

---
+++ libs/system/BootStrap.php
@@ -0,0 +1,64 @@
+<?php
+//use src\controller\RolesController;
+namespace libs\system;
+session_start();
+class BootStrap
+{
+    
+    public function __construct()
+    {
+        if(isset($_SESSION['user']))
+        {
+            if(isset($_GET['url']))
+            {
+                //echo $_GET['url'];
+                $url = explode("/", $_GET['url']);
+                $controller_file = 'src/controller/'.$url[0].'Controller.php';
+                if(file_exists($controller_file))
+                {
+                    require_once $controller_file;
+                    $file = $url[0]."Controller";
+                    $controller_object = new $file();
+                    if(isset($url[2]))
+                    {
+                        $method = $url[1];
+                        if(method_exists($controller_object, $method))
+                        {
+                            $controller_object->$method($url[2]);
+                        }
+                        else
+                        {
+                            die($method." n'existe pas dans le controller ".$file);
+                        }
+                    }
+                    else if(isset($url[1]))
+                    {
+                        $method = $url[1];
+                        if(method_exists($controller_object, $method))
+                        {
+                            $controller_object->$method();
+                        }
+                        else
+                        {
+                            die($method." n'existe pas dans le controller ".$file);
+                        }
+                    }
+
+                }
+                else
+                {
+                    die($controller_file." n'existe pas");
+                }
+            }
+            else
+            {
+                $ac = new \AcceuilController();
+
+                $ac->acceuil();
+            }
+        }else{
+            $uc = new \UserController();
+            $uc->authentification();
+        }
+    }
+}
