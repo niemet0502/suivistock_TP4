26af1444d842b1a5cbde40d7dd971ad5b33823b9
niemet0502 <vincentmarius8@gmail.com>
2021-02-24 00:29:09 +0100

    main :: added content in UserController

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -1,4 +1,35 @@
-<?php 
-  class UserController{
-    
-  }
+<?php
+
+    use libs\system\Controller;
+    use src\model\UserDb;
+    //session_start();
+    class UserController extends Controller
+    {
+        public function authentification()
+        {
+            if(isset($_POST['vefUser']))
+            {
+                extract($_POST);
+                $udb = new UserDb();
+                $user = $udb->getUser($login, $mdp);
+                if($user != null)
+                {
+                    $_SESSION['user'] = $user[0]->getNom()." ".$user[0]->getPnom();
+                    $this->view->load("acceuil/acceuil");
+                }else{
+                    $error = "LOGIN OU MOT DE PASSE INCORRECT !!!";
+                    return $this->view->load("connexion/authentification",$error);
+                }
+            }else{
+                return $this->view->load("connexion/authentification");
+            }
+        }
+
+        public function disconnect(){
+            $_SESSION['user'] = array();
+            session_unset();
+            header('location:/suivistock/');
+        }
+
+    }
+?>
