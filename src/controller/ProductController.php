905939db1536c828283734ccc667dc51c6c0d907
niemet0502 <vincentmarius8@gmail.com>
2021-03-01 06:24:00 +0100

    main :: get Products List  file

--- src/controller/ProductController.php
+++ src/controller/ProductController.php
@@ -1,4 +1,12 @@
 <?php
-class ProductController{
+  use libs\system\Controller;
+  use src\model\ProductDb;
 
+class ProductController extends Controller{
+  public function getList()
+  {
+      $cdb = new ProductDb();
+      $products = $cdb->getProduct();
+      return $this->view->load("product/liste",$products);
+  }
 }
