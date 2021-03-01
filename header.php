c550d35e8d85e34ce34874cb9fe77c1b6a13e572
niemet0502 <vincentmarius8@gmail.com>
2021-03-01 06:23:38 +0100

    main :: created header file

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Banque du Peuple</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Favicons -->
+    <link href="/suivistock/public/img/favicon.png" rel="icon">
+    <link href="/suivistock/public/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Oswald:400,300,700|Lato:400,700" rel="stylesheet">
+
+    <!-- Bootstrap CSS File -->
+    <link href="/suivistock/public/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="/suivistock/public/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/suivistock/public/lib/fancybox/fancybox.css" rel="stylesheet">
+
+    <!-- Main Stylesheet File -->
+    <link href="/suivistock/public/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<!-- Menu -->
+<nav class="menu" id="theMenu">
+    <div class="menu-wrap">
+        <h1 class="logo"><a href="#home" class="smoothscroll">Gestion Produit</a></h1>
+        <a href="/suivistock/Acceuil/acceuil">Acceuil</a>
+        <a href="/suivistock/Product/getList">Gestion des Produits</a>
+        <a href="/suivistock/User/disconnect">Deconnexion</a>
+    </div>
+
+    <!-- Menu button -->
+    <div id="menuToggle"><i class="fa fa-bars"></i></div>
+</nav>
