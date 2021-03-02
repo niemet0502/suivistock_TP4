b6214158b64da8a7cae223ddcdb0f19abfbea949
niemet0502 <vincentmarius8@gmail.com>
2021-03-02 10:30:16 +0100

    main :: update footer file

--- footer.php
+++ footer.php
@@ -1,16 +1,42 @@
-<script src="/suivistock/public/lib/jquery/jquery.min.js"></script>
-<script src="/suivistock/public/lib/bootstrap/js/bootstrap.min.js"></script>
-<script src="/suivistock/public/lib/php-mail-form/validate.js"></script>
-<script src="/suivistock/public/lib/easing/easing.min.js"></script>
-<script src="/suivistock/public/lib/stellar/stellar.min.js"></script>
-<script src="/suivistock/public/lib/fancybox/fancybox.js"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<!-- Template Main Javascript File -->
-<script src="/suivistock/public/js/main.js"></script>
-<script src="/suivistock/public/js/jquery.js"></script>
-<script src="/suivistock/public/js/script.js"></script>
-
+<footer class="footer pt-0">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-6">
+            <div class="copyright text-center  text-lg-left  text-muted">
+              &copy; 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+              <li class="nav-item">
+                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+              </li>
+              <li class="nav-item">
+                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <!-- Argon Scripts -->
+  <!-- Core -->
+  <script src="/suivistock/public/vendor/jquery/dist/jquery.min.js"></script>
+  <script src="/suivistock/public/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="/suivistock/public/vendor/js-cookie/js.cookie.js"></script>
+  <script src="/suivistock/public/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+  <script src="/suivistock/public/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+  <!-- Optional JS -->
+  <script src="/suivistock/public/vendor/chart.js/dist/Chart.min.js"></script>
+  <script src="/suivistock/public/vendor/chart.js/dist/Chart.extension.js"></script>
+  <!-- Argon JS -->
+  <script src="/suivistock/public/js/argon.js?v=1.2.0"></script>
 </body>
+
 </html>
