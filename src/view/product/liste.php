2eb45ed83c327484f512e21d997b9867f01e1cc6
niemet0502 <vincentmarius8@gmail.com>
2021-03-02 11:00:04 +0100

    main :: displayed product list

--- src/view/product/liste.php
+++ src/view/product/liste.php
@@ -1,37 +1,70 @@
 <?php
     include_once "header.php";
 ?>
+<div class="row d-flex align-items-center justify-content-center">
+  <!-- Dark table -->
+  <div class="row col-md-11">
+    <div class="col">
+      <div class="card bg-default shadow">
+        <div class="card-header bg-transparent border-0">
+          <h3 class="text-white mb-0">Liste des Produits</h3>
+        </div>
+        <div class="table-responsive">
+          <table class="table align-items-center table-dark table-flush">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col" class="sort" data-sort="name">#</th>
+                <th scope="col" class="sort" data-sort="budget">Reference</th>
+                <th scope="col" class="sort" data-sort="status">Quantite en stock</th>
+                <th scope="col">NOM</th>
+                <th scope="col" class="sort" data-sort="completion">Actions</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody class="list">
 
-<div class="col-md-10 offset-1 mt-5">
-<table class="table mt-5">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Reference</th>
-      <th scope="col">Nom</th>
-      <th scope="col">Quantite en stock</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php 
-      if($data){
-        foreach($data as $product){
-          ?>
-            <tr>
-              <th scope="row"><?= $product->getId() ?></th>
-              <td><?= $product->getRef() ?></td>
-              <td><?= $product->getNom() ?></td>
-              <td> <?= $product->getQteStock() ?> </td>
-              <td>
-                <button class="btn btn-warning">MODIFIER</button>
-                <button class="btn btn-danger">SUPPRIMER</button>
-              </td>
-            </tr>
-          <?php
-        }
-      }
-    ?>
-  </tbody>
-</table>
+            <?php 
+              if($data){
+                foreach($data as $product){
+                  ?>
+                    <tr>
+                <th scope="row">
+                <?= $product->getId() ?>
+                </th>
+                <td class="budget">
+                <?= $product->getRef() ?>
+                </td>
+                <td>
+                <?= $product->getQteStock() ?>
+                </td>
+                <td>
+                <?= $product->getNom() ?>
+                </td>
+                <td class="text-right">
+                  <div class="dropdown">
+                    <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                      <a class="dropdown-item" href="#">Supprimer</a>
+                      <a class="dropdown-item" href="#">Modifier</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+
+                  <?php
+                }
+              }
+            ?>  
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+      
+<?php 
+  include_once "footer.php"
+?>
